088c2e62a53c245c62926b8e5855e70e683b70a5
LukeJansen <luke.jansen@students.plymouth.ac.uk>
2017-11-20 12:38:26 +0000

    Added data.js

---
+++ ,Assignment/js/data.js
@@ -0,0 +1,16 @@
+var roomArray = [
+	{
+		title:'room 1',
+		text:'you are standing room 1',
+		choices:[
+			{
+				text:'Go to room 2',
+				index:1
+			},
+			{
+				text:'Go to room 3',
+				index:2
+			}
+		]
+	}
+]
